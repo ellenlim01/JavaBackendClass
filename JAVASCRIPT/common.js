8cbabd4934c5c3451edca5bebb9252acd6def0c2
ellenlim01 <ellenlim02@naver.com>
2021-03-19 15:57:34 +0900

    day5

--- JAVASCRIPT/common.js
+++ JAVASCRIPT/common.js
@@ -12,3 +12,14 @@ function clkLogin() {
   }
   location.href = "main.html";
 }
+
+function join() {
+  var pwElem1 = document.getElementById("pw1");
+  var pwElem2 = document.getElementById("pw2");
+
+  if (pwElem1.value !== pwElem2.value) {
+    alert("비밀번호를 확인해주세요.");
+    return;
+  }
+  location.href = "login.html";
+}
