43a37b16ab871bf03ec44bd40da6125cddeac8fb
ellenlim01 <ellenlim02@naver.com>
2021-03-21 22:15:47 +0900

    day7

--- JAVASCRIPT/exam2common.js
+++ JAVASCRIPT/exam2common.js
@@ -1,13 +1,33 @@
-function clkLogin() {
-  var txtIdElem = document.getElementById("txtId");
-  if (txtIdElem === "") {
-    alert("로그인을 할 수 없습니다.");
-    return;
-  }
-  var txtPwElem = document.getElementById("txtPw");
-  if (txtPwElem === "") {
-    alert("로그인을 할 수 없습니다.");
-    return;
+function join() {
+  location.href="join.html"
+}
+
+function login() {
+  var loginId = document.getElementById("loginId");
+  var loginPw = document.getElementById("loginPw");
+
+  if(loginId.value === '' || loginPw.value === '') {
+      alert("로그인을 할 수 없습니다.");
+  } else {
+      location.href="main.html"
   }
 }
-<a href="javascript:history.back();">뒤로가기</a>
+
+function goToBack() {
+  history.back();
+}
+
+function joinLogin() {
+  var joinId = document.getElementById("joinbId");
+  var joinPw = document.getElementById("joinPw");
+  var joinPw2 = document.getElementById("joinPw2");
+  if ( joinId.value === '' ||  joinPw.value === '' || joinPw2.value === '') {
+      alert("회원가입을 할 수 없습니다.");
+  } else {
+      if (joinPw.value !== joinPw2.value) {
+          alert('비밀번호를 확인해주세요.');
+      } else {
+          location.href="login.html"
+      }
+  }
+}
